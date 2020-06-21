537ffe1a6063346b318bb2a1458d933b4bb2b2c9
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-06-21 17:24:35 +0200

    Added User entity repository

---
+++ src/main/java/com/easetest/website/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.easetest.website.repository;
+
+import com.easetest.website.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    // JPA creates implementation according to generic type: User, name of method and method parameter
+    Optional<User> findByUserName(String userName);
+}
+
