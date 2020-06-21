b8e813ad2c920d06a7dd4a2c476bb16bc36af6fb
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-06-21 17:19:31 +0200

    Added main webapp class

---
+++ src/main/java/com/easetest/website/WebsiteApplication.java
@@ -0,0 +1,16 @@
+package com.easetest.website;
+
+import com.easetest.website.repository.UserRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class WebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteApplication.class, args);
+    }
+
+}
