12d5bcaeb58d8522faf80715fb53a79c2ebabf65
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-06-24 21:48:32 +0200

    Added validation annotations to test entity

--- src/main/java/com/easetest/website/model/Test.java
+++ src/main/java/com/easetest/website/model/Test.java
@@ -2,8 +2,11 @@ package com.easetest.website.model;
 
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,17 +21,27 @@ public class Test {
     @GenericGenerator(name = "native", strategy = "native")
     private int id;
     @EqualsAndHashCode.Exclude
+    @NotEmpty(message = "Field cannot be empty.")
     private String testName;
     @EqualsAndHashCode.Exclude
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @NotNull(message = "Field cannot be empty.")
+    @Future(message = "Field must contain a future date.")
     private LocalDateTime startTime;
     @EqualsAndHashCode.Exclude
-    private int testTime; //minutes
+    @NotNull(message = "Field cannot be empty")
+    @Min(value = 5, message = "Minimum value is 5.")
+    @Max(value = 120, message = "Maximum value is 120.")
+    private Integer testTime; //minutes
     @EqualsAndHashCode.Exclude
     private boolean multipleAnswers;
+    //possibility to user part of all questions in test
     @EqualsAndHashCode.Exclude
-    private int numberOfQuestions;
+    @Min(value = 0, message = "Minimum value is 0.")
+    @Max(value = 100, message = "Maximum value is 100.")
+    @NotNull(message = "Field cannot be empty")
+    private Integer numberOfQuestions;
 
-    //mapa pytań (możliwość losowania z całej puli/mapy, stąd pole numberOfQuestions)
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
