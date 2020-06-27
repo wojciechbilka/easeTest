c41aef1326b1856aa10c31d9c9a868af872aa007
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-06-27 23:25:49 +0200

    Completed Question entity with Answers as objects

--- src/main/java/com/easetest/website/model/Question.java
+++ src/main/java/com/easetest/website/model/Question.java
@@ -1,9 +1,6 @@
 package com.easetest.website.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -22,21 +19,29 @@ public class Question {
     private int id;
 
     @EqualsAndHashCode.Exclude
-    private Integer questionNumber;
+    private int questionNumber;
     private String questionBody;
 
-    @ElementCollection
-    @CollectionTable(name = "question_answers_mapping",
-            joinColumns = {@JoinColumn(name = "question_id", referencedColumnName = "id")})
-    @MapKeyColumn(name = "answer_number")
-    @Column(name = "answer")
-    private Map<Integer, String> answers;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    //TODO add orphan removal (adding new answer in case of editing)
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "question_answers_mapping",
+            joinColumns = {@JoinColumn(name = "question_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "answer_id", referencedColumnName = "id")})
+    @MapKey(name = "answerNumber")
+    private Map<Integer, Answer> answers = new HashMap<>();
 
-    public void addAnswer(Integer num, String answer) {
+
+    public void setAnswer(Integer num, String answer, boolean correct) {
         if(answers == null) {
             answers = new HashMap<>();
         }
-        answers.put(num, answer);
+        Answer a = answers.get(num) != null ? answers.get(num) : new Answer();
+        a.setAnswerNumber(num);
+        a.setText(answer);
+        a.setCorrect(correct);
+        answers.put(num, a);
     }
 
     public void removeAnswer(Integer num) {
