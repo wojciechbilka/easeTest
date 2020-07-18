3a8db461c57972cdfe08f53dd9d7264ee7a96345
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-07-18 14:19:30 +0200

    Removed unused imports

--- src/main/java/com/easetest/website/config/WebConfig.java
+++ src/main/java/com/easetest/website/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.easetest.website.config;
 
 import com.easetest.website.converters.StringToAnswerConverter;
-import com.easetest.website.service.QuestionService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
